496d38e7bb7bf9648326f20c28be92ce79117839
valfer18 <167267939+valfer18@users.noreply.github.com>
2024-10-24 17:59:28 -0600

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const burgerMenu = document.querySelector('.burger-menu');
-    const nav = document.querySelector('.main-nav');
-
-    burgerMenu.addEventListener('click', () => {
-      nav.classList.toggle('open');
-    });
-  });
